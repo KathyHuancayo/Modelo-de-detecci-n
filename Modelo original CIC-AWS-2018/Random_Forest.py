287293c2258abb2cbf0d35aaea108a6442e267fc
KathyHuancayo <46538680+KathyHuancayo@users.noreply.github.com>
2020-05-29 16:00:36 -0500

    Update Random_Forest.py

--- Modelo original CIC-AWS-2018/Random_Forest.py
+++ Modelo original CIC-AWS-2018/Random_Forest.py
@@ -56,7 +56,7 @@ print("CANTIDAD DE DATOS PARA CADA CLASE: ")
 print(df.groupby('Output').size())
 
 #OBTENIENDO DATOS DE TEST(PRUEBA) Y TRAIN (ENTRENAMIENTO)
-X_train, X_test, y_train, y_test = train_test_split( df1, y, test_size = 0.4,
+X_train, X_test, y_train, y_test = train_test_split( df1, y, test_size = 0.2,
                                                     random_state =0 )
 
 X_train.shape
