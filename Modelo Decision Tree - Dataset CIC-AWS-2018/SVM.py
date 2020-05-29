33832dd73f1020f2f2085e99d5d683b2833dc835
KathyHuancayo <46538680+KathyHuancayo@users.noreply.github.com>
2020-05-29 15:46:36 -0500

    Update SVM.py

--- Modelo Decision Tree - Dataset CIC-AWS-2018/SVM.py
+++ Modelo Decision Tree - Dataset CIC-AWS-2018/SVM.py
@@ -25,7 +25,7 @@ X = datos[['Dst_Port','Protocol','Flow_Duration','Tot_Fwd_Pkts','Tot_Bwd_Pkts',
           'Bwd_Pkt_Len_Mean','Flow_Byts/s','Flow_Pkts/s','Fwd_Pkts/s','Bwd_Pkts/s','Subflow_Fwd_Byts','Subflow_Bwd_Byts','Subflow_Bwd_Pkts','Subflow_Fwd_Pkts']]  
 
 y=datos['Output']
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.4)
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2)
 X_train.shape
 X_test.shape
 y_train.shape
