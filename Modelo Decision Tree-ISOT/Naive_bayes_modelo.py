63bc7048d649d96e9fc759be7e8e9e5507464808
KathyHuancayo <46538680+KathyHuancayo@users.noreply.github.com>
2020-05-29 15:48:59 -0500

    Update Naive_bayes_modelo.py

--- Modelo Decision Tree-ISOT/Naive_bayes_modelo.py
+++ Modelo Decision Tree-ISOT/Naive_bayes_modelo.py
@@ -33,7 +33,7 @@ y=df.Output
 
 #DIVIDIR EL DATASET EN CONJUNTO DE ENTRENAMIENTO Y CONJUNTO DE PRUEBA
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(df1, y,test_size=0.4,random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(df1, y,test_size=0.2,random_state = 0)
 
 start = time.time()
 # Fitting Naive Bayes to the Training set
