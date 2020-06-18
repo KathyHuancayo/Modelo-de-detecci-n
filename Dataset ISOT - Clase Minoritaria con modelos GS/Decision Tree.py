a2754158bde1f1df6b8305a5e700f95d6d931b2b
KathyHuancayo <46538680+KathyHuancayo@users.noreply.github.com>
2020-06-18 07:10:29 -0500

    Add files via upload

---
+++ Dataset ISOT - Clase Minoritaria con modelos GS/Decision Tree.py
@@ -0,0 +1,167 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 22 10:46:20 2019
+
+@author: user
+"""
+#PROCESAMIENTO
+import time
+import os
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+plt.style.use('fivethirtyeight')
+import warnings
+warnings.filterwarnings('ignore')
+#CARGAR LOS DATOS
+os.getcwd()
+
+os.chdir("C://Users//Sony//Desktop//TESIS 2//isot_app_and_botnet_dataset//botnet_data")
+
+df = pd.read_csv('Clase_Minoritaria.csv', keep_default_na=False, na_values=[""])
+df.head(10)
+
+#PREPARAR LOS DATOS
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score, cross_val_predict, KFold
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn import tree
+import sklearn.metrics
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn import metrics
+
+df1 = df[['Src_Port','Dst_Port','Protocol','Flow_Duration','Tot_Fwd_Pkts','Tot_Bwd_Pkts',
+          'TotLen_Fwd_Pkts','TotLen_Bwd_Pkts','Fwd_Pkt_Len_Mean','Fwd Pkt Len Max','Fwd Pkt Len Min',
+          'Bwd_Pkt_Len_Mean','Flow_Byts/s','Flow_Pkts/s','Fwd_Pkts/s','Bwd_Pkts/s','Subflow_Fwd_Byts','Subflow_Bwd_Byts','Subflow_Bwd_Pkts','Subflow_Fwd_Pkts']]   
+
+features =  ['Src_Port','Dst_Port','Protocol','Flow_Duration','Tot_Fwd_Pkts','Tot_Bwd_Pkts',
+             'TotLen_Fwd_Pkts','TotLen_Bwd_Pkts','Fwd_Pkt_Len_Mean','Fwd Pkt Len Max','Fwd Pkt Len Min',
+             'Bwd_Pkt_Len_Mean','Flow_Byts/s','Flow_Pkts/s','Fwd_Pkts/s','Bwd_Pkts/s','Subflow_Fwd_Byts','Subflow_Bwd_Byts','Subflow_Bwd_Pkts','Subflow_Fwd_Pkts']
+
+y=df.Output
+
+#OBTENIENDO DATOS DE TEST(PRUEBA) Y TRAIN (ENTRENAMIENTO)
+X_train, X_test, y_train, y_test = train_test_split( df1, y, test_size = 0.2,
+                                                    random_state =0 )
+
+X_train.shape
+X_test.shape
+y_train.shape
+y_test.shape
+
+
+#CREAR EL MODELO
+start = time.time()
+model=DecisionTreeClassifier(ccp_alpha=0.0, class_weight='balanced', criterion='gini',
+                       max_depth=20, max_features=20, max_leaf_nodes=None,
+                       min_impurity_decrease=0.0, min_impurity_split=None,
+                       min_samples_leaf=1, min_samples_split=2,
+                       min_weight_fraction_leaf=0.0, presort='deprecated',
+                       random_state=0, splitter='best')  
+model=model.fit(X_train,y_train)
+end = time.time()
+print ("Decision Tree", end - start)
+predictions = model.predict(X_test)
+
+# VALIDAR MODELO
+name='ARBOL DE DECISIÓN'
+results = cross_val_score(model, X_train, y_train,scoring='accuracy', cv=5)
+print("Accuracy: %.3f%% (%.3f%%)" % (results.mean()*100, results.std()*100))
+print ('CROSS-VALIDATION SCORES:')
+print(results)
+ 
+#------------------------------REPORTE DE RESULTADOS POR CLASE-------------------------------
+print(classification_report(y_test,predictions))
+
+print("PRECISIÓN PARA DETECTAR DIFERENTES MUESTRAS DE BOTNET ", metrics.precision_score(y_test, predictions,average=None)*100)
+
+print("RECALL PARA DETECTAR DIFERENTES MUESTRAS DE BOTNET: ", metrics.recall_score(y_test, predictions, average=None)*100)
+
+print ("EXACTITUD DEL MODELO:  ", sklearn.metrics.accuracy_score(y_test, predictions, normalize=True)*100)
+
+#------------------------------MATRIZ DE CONFUSIÓN PARA VALIDACIÓN-------------------------------
+x_axis_labels = ['Blackout','Blue','Liphyra','Black Energy','Zyklon'] # labels for x-axis
+y_axis_labels = ['Blackout','Blue','Liphyra','Black Energy','Zyklon'] # labels for y-axis
+
+print("MATRIZ DE CONFUSIÓN PARA VALIDACION: ")
+cm1=confusion_matrix(y_test, predictions)
+print(cm1)
+plt.figure(figsize=(12, 12))
+sns.heatmap(cm1,xticklabels=x_axis_labels, yticklabels=y_axis_labels ,annot=True, fmt="d");
+plt.title("Confusion matrix")
+plt.ylabel('True class')
+plt.xlabel('Predicted class')
+plt.show()
+
+
+#------------1.GRÁFICO DEL ÁRBOL DE DECISIÓN--------------------------------
+#DIBUJAR EL ÁRBOL
+from sklearn import tree
+from io import StringIO
+from IPython.display import Image
+#PINTAR EL ÁRBOL
+out = StringIO()
+tree.export_graphviz(model, out_file='treeMacarena.dot') 
+
+ # PREDECIR BOTNET BLACKOUT:
+X_test = pd.DataFrame(columns=('Src_Port','Dst_Port','Protocol','Flow_Duration','Tot_Fwd_Pkts','Tot_Bwd_Pkts',
+          'TotLen_Fwd_Pkts','TotLen_Bwd_Pkts','Fwd_Pkt_Len_Mean','Fwd Pkt Len Max','Fwd Pkt Len Min',
+          'Bwd_Pkt_Len_Mean','Flow_Byts/s','Flow_Pkts/s','Fwd_Pkts/s','Bwd_Pkts/s','Subflow_Fwd_Byts','Subflow_Bwd_Byts','Subflow_Bwd_Pkts','Subflow_Fwd_Pkts','Output'))
+
+X_test.loc[0] = (64181,53,17,205,1,1,38,122,38,38,38,122,780487.8049,9756.097561,4878.04878,4878.04878,19,61,0,0,0)
+y_pred = model.predict(X_test.drop(['Output'], axis = 1))
+print("Prediccion: " + str(y_pred))
+y_proba = model.predict_proba(X_test.drop(['Output'], axis = 1))
+print("Probabilidad de Acierto: " + str(np.round(np.asarray(y_proba[0][y_pred])* 100, 2)))
+
+# PREDECIR BOTNET BLUE:
+X_test = pd.DataFrame(columns=('Src_Port','Dst_Port','Protocol','Flow_Duration','Tot_Fwd_Pkts','Tot_Bwd_Pkts',
+          'TotLen_Fwd_Pkts','TotLen_Bwd_Pkts','Fwd_Pkt_Len_Mean','Fwd Pkt Len Max','Fwd Pkt Len Min',
+          'Bwd_Pkt_Len_Mean','Flow_Byts/s','Flow_Pkts/s','Fwd_Pkts/s','Bwd_Pkts/s','Subflow_Fwd_Byts','Subflow_Bwd_Byts','Subflow_Bwd_Pkts','Subflow_Fwd_Pkts','Output'))
+
+X_test.loc[0] = (54301,53,17,217,1,1,34,118,34,34,34,118,700460.8295,9216.589862,4608.294931,4608.294931,17,59,0,0,1)
+y_pred = model.predict(X_test.drop(['Output'], axis = 1))
+print("Prediccion: " + str(y_pred))
+y_proba = model.predict_proba(X_test.drop(['Output'], axis = 1))
+print("Probabilidad de Acierto: " + str(np.round(np.asarray(y_proba[0][y_pred])* 100, 2)))
+
+
+# PREDECIR BOTNET LIPHYRA:
+
+X_test = pd.DataFrame(columns=('Src_Port','Dst_Port','Protocol','Flow_Duration','Tot_Fwd_Pkts','Tot_Bwd_Pkts',
+          'TotLen_Fwd_Pkts','TotLen_Bwd_Pkts','Fwd_Pkt_Len_Mean','Fwd Pkt Len Max','Fwd Pkt Len Min',
+          'Bwd_Pkt_Len_Mean','Flow_Byts/s','Flow_Pkts/s','Fwd_Pkts/s','Bwd_Pkts/s','Subflow_Fwd_Byts','Subflow_Bwd_Byts','Subflow_Bwd_Pkts','Subflow_Fwd_Pkts','Output'))
+
+X_test.loc[0] = (62238,53,17,246,1,1,37,121,37,37,37,121,642276.4228,8130.081301,4065.04065,4065.04065,18,60,0,0,2)
+y_pred = model.predict(X_test.drop(['Output'], axis = 1))
+print("Prediccion: " + str(y_pred))
+y_proba = model.predict_proba(X_test.drop(['Output'], axis = 1))
+print("Probabilidad de Acierto: " + str(np.round(np.asarray(y_proba[0][y_pred])* 100, 2)))
+
+# PREDECIR BOTNET BLACK ENERGY:
+X_test = pd.DataFrame(columns=('Src_Port','Dst_Port','Protocol','Flow_Duration','Tot_Fwd_Pkts','Tot_Bwd_Pkts',
+          'TotLen_Fwd_Pkts','TotLen_Bwd_Pkts','Fwd_Pkt_Len_Mean','Fwd Pkt Len Max','Fwd Pkt Len Min',
+          'Bwd_Pkt_Len_Mean','Flow_Byts/s','Flow_Pkts/s','Fwd_Pkts/s','Bwd_Pkts/s','Subflow_Fwd_Byts','Subflow_Bwd_Byts','Subflow_Bwd_Pkts','Subflow_Fwd_Pkts','Output'))
+
+X_test.loc[0] = (52799,53,17,290,1,1,32,116,32,32,32,116,510344.8276,6896.551724,3448.275862,3448.275862,16,58,0,0,3)
+y_pred = model.predict(X_test.drop(['Output'], axis = 1))
+print("Prediccion: " + str(y_pred))
+y_proba = model.predict_proba(X_test.drop(['Output'], axis = 1))
+print("Probabilidad de Acierto: " + str(np.round(np.asarray(y_proba[0][y_pred])* 100, 2)))
+
+
+
+# PREDECIR BOTNET ZYKLON:
+X_test = pd.DataFrame(columns=('Src_Port','Dst_Port','Protocol','Flow_Duration','Tot_Fwd_Pkts','Tot_Bwd_Pkts',
+          'TotLen_Fwd_Pkts','TotLen_Bwd_Pkts','Fwd_Pkt_Len_Mean','Fwd Pkt Len Max','Fwd Pkt Len Min',
+          'Bwd_Pkt_Len_Mean','Flow_Byts/s','Flow_Pkts/s','Fwd_Pkts/s','Bwd_Pkts/s','Subflow_Fwd_Byts','Subflow_Bwd_Byts','Subflow_Bwd_Pkts','Subflow_Fwd_Pkts','Output'))
+
+X_test.loc[0] = (64628,53,17,260,1,1,36,120,36,36,36,120,600000,7692.307692,3846.153846,3846.153846,18,60,0,0,4)
+y_pred = model.predict(X_test.drop(['Output'], axis = 1))
+print("Prediccion: " + str(y_pred))
+y_proba = model.predict_proba(X_test.drop(['Output'], axis = 1))
+print("Probabilidad de Acierto: " + str(np.round(np.asarray(y_proba[0][y_pred])* 100, 2)))
+
